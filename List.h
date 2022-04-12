20b4cdae365ff00100ab7533dc553fc2aadde6aa
aryunin <im.aryunin@gmail.com>
2022-04-12 21:07:52 +0300

    Added list class

--- List.h
+++ List.h
@@ -5,3 +5,13 @@ struct Elem {
     T data;
     Elem<T>* next;
 };
+
+template <typename T>
+class List {
+private:
+    Elem<T>* head;
+    Elem<T>* last;
+public:
+    List() : head{nullptr}, last{nullptr} { }
+
+};
