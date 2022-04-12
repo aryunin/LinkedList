22982c9a33a4093bb7a12bce8dbd2672ca274d48
aryunin <im.aryunin@gmail.com>
2022-04-12 23:37:42 +0300

    Added iterator class

--- List.h
+++ List.h
@@ -2,16 +2,39 @@
 
 template <typename T>
 class List;
+template <typename T>
+class Iterator;
 
 template <typename T>
 class Elem {
     friend class List<T>;
+    friend class Iterator<T>;
 private:
     T data;
     Elem<T>* next;
 public:
     explicit Elem(const T& _data) : data{_data}, next{nullptr} { }
     Elem(const T& _data, Elem<T>* _next) : data{_data}, next{_next} { }
+};
+
+template <typename T>
+class Iterator {
+private:
+    Elem<T>* current;
+public:
+    explicit Iterator(Elem<T>* elem) : current{elem} {
+        if(!current) throw std::out_of_range{"empty iterator"};
+    }
+    T get() const {
+        return current->data;
+    }
+    void next() {
+        if(!current->next) throw std::out_of_range{"next is null"};
+        current = current->next;
+    }
+    bool hasNext() const {
+        return current->next;
+    }
 };
 
 template <typename T>
@@ -24,7 +47,10 @@ public:
     void add(const T&);
     void insert(const T&, size_t);
     void del(size_t);
-    T get(size_t);
+    T get(size_t) const;
+    Iterator<T> firstIterator() const {
+        return Iterator<T>{head};
+    }
 };
 
 template <typename T>
@@ -63,7 +89,7 @@ void List<T>::del(const size_t idx) {
 }
 
 template <typename T>
-T List<T>::get(const size_t idx) {
+T List<T>::get(const size_t idx) const {
     Elem<T>* current{head};
     for(size_t i {0}; i < idx; i++) {
         if(!current->next) throw std::out_of_range{"invalid index"};
